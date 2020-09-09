6d5c041ab03c16e6254d96fb538b02cea6a16c0b
mrnafstad <halvornafstad@hotmail.com>
2020-09-09 13:39:44 +0200

    build

---
+++ precache-manifest.9f6df950a83325caee01284eaeafa645.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd02e6f69bbdcec0134",
+    "url": "/Bike-tracker/css/chunk-vendors.665112ee.css"
+  },
+  {
+    "revision": "d21a0ebe1cce1772619f01d8ad78ab37",
+    "url": "/Bike-tracker/index.html"
+  },
+  {
+    "revision": "b7af2d772b51c144e57f",
+    "url": "/Bike-tracker/js/app.77409b7a.js"
+  },
+  {
+    "revision": "cfd02e6f69bbdcec0134",
+    "url": "/Bike-tracker/js/chunk-vendors.97304afb.js"
+  },
+  {
+    "revision": "07e11183699cc3fdba2247dd44e4a016",
+    "url": "/Bike-tracker/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Bike-tracker/robots.txt"
+  }
+]);
