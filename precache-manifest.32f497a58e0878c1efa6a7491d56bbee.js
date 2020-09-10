39abab021711a9417def0021db0b568aa56043f8
mrnafstad <halvornafstad@hotmail.com>
2020-09-10 18:36:45 +0200

    adding dist subtree

---
+++ precache-manifest.32f497a58e0878c1efa6a7491d56bbee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e347db5c7c276eb48f0d",
+    "url": "Bike-tracker/css/chunk-vendors.aca02bfb.css"
+  },
+  {
+    "revision": "a9102b653d2ee4c4bee178546015714c",
+    "url": "Bike-tracker/index.html"
+  },
+  {
+    "revision": "4497eb0cbb384689237c",
+    "url": "Bike-tracker/js/app.18507ad1.js"
+  },
+  {
+    "revision": "e347db5c7c276eb48f0d",
+    "url": "Bike-tracker/js/chunk-vendors.e696fef3.js"
+  },
+  {
+    "revision": "07e11183699cc3fdba2247dd44e4a016",
+    "url": "Bike-tracker/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "Bike-tracker/robots.txt"
+  }
+]);
